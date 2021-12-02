a16fd5e7baaaec087ccd142ba296183d235467b9
SamuliM <t9musa00@students.oamk.ficode .git config --global user.email t9musa00@students.oamk.fi>
2021-12-02 18:34:02 +0200

    added login and register implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React, { useState } from 'react'
 import Header from './components/Header.js';
 import Managerfrontpage from './components/Managerfrontpage.js';
 import Footer from './components/Footer.js';
@@ -8,24 +8,57 @@ import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Frontpage from './components/Frontpage';
 import TempRestManagerList from './components/TempRestManagerList';
 import ManagerAccountPage from './components/ManagerAccountPage';
+import Registerpage from './components/Registerpage';
+import Loginpage from './components/Loginpage';
+import Payload from './components/Payload.js';
+
+const jwtFromLocalStorage = window.localStorage.getItem('localStorageJWT');
 
 
 function App() {
+
+  const [userJWT, setUserJWT] = useState(jwtFromLocalStorage);
+
+
+    //routes which are accessable only when user is not logged in
+    let accessableRoutes = <>
+      <Route path="/registerpage"element={<Registerpage />}/>  
+      <Route path="/loginpage"element={<Loginpage login={ newJWToken => {
+        setUserJWT(newJWToken)
+      window.localStorage.setItem('localStorageJWT', newJWToken)
+      }
+      }/>}/>  
+    </>
+
+    //these routes are accessable only when user is logged in
+    if (userJWT != null) {
+      accessableRoutes =
+    <>
+    <Route path="/restmanagerlist" element={<TempRestManagerList/>}/>
+    <Route path="/payload" element={<Payload jwt={userJWT} />}/>
+    </>
+    }
+
+
+
+
   return (
     <div >
 
 
-<Header/>
+<Header userLoggedIn={userJWT != null} logout={()=> {
+  setUserJWT(null)
+  window.localStorage.removeItem('localStorageJWT');
+}}/>
 
 <Router>
       <div style={{ display:"flex", justifyContent: "space-around" }}>
-        <Link to='/'>Frontpage</Link>
-        <Link to='/managerfrontpage'>Manager frontpage </Link>
-        <Link to='/restmanagerlist'>Restaurant manager list</Link>
 
-        {/* Commented out becasue they don't have functionality yet */}
-
-        {/* <Link to='/restaurantaccountpage'>Restaurant account page</Link>
+      <Link to='/payload'>Payload</Link> 
+        {/* <Link to='/restmanagerlist'>Restaurant manager list</Link> 
+        <Link to='/registerpage'>Register page</Link>
+        <Link to='/loginpage'>Login page</Link>
+         <Link to='/restaurantaccountpage'>Restaurant account page</Link>
         <Link to='/managerorderhistorypage'>Manager Order History</Link>
         <Link to='/searchresultpage'>Search Result Page</Link>
         <Link to='/foodcategoriespage'>Food Categories Page</Link>
@@ -35,12 +68,15 @@ function App() {
       </div>
       <SearchbarLocation />
     <Routes>
-    <Route path="/" element={<Frontpage/>}/>
+    <Route path="/" element={<Frontpage userLoggedIn={userJWT != null}/>}/>
     <Route path="/managerfrontpage" element={<Managerfrontpage/>}/>
       <Route path="/*" element={<Errorpage/>}/>
-      <Route path="/restmanagerlist" element={<TempRestManagerList/>}/>
+      
       <Route path="/manageraccpage" element={<ManagerAccountPage/>}/>
       <Route path="manageraccpage/:params" element={<ManagerAccountPage/>}/>
+      {accessableRoutes}
+
+      
     </Routes>
 
    </Router>
---
+++ src/components/Loginpage.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import api from '../api/config_login';
+import { useNavigate } from 'react-router-dom';
+
+function Loginpage(props) {
+
+    const navigate = useNavigate();
+    const [ logInSetUpState, setlogInSetUpState] = useState("idle")
+
+    const handleLogIn = (event) => {
+        event.preventDefault();
+
+        setlogInSetUpState("processing");
+        const customerLogIn =  async () => {
+            try {const res = await api.post('', 
+            null, 
+            {
+                auth: {
+                username: event.target.email.value,
+                password: event.target.password.value
+                }
+            }
+            
+            );
+            //successful login
+            setlogInSetUpState("SignInSuccessful");
+            console.log(res);
+            const JWToken = res.data.jwt;
+            props.login(JWToken);
+
+            setTimeout(() => {
+                navigate('/', {replace: true})
+            }, 1500);
+
+                    
+                    //resetting navigation history?
+                   /*  {state:{
+                        email:event.target.email.value
+                        }} */
+    
+            } catch (error) {
+                setlogInSetUpState("SignInFailed");
+                console.log(error);
+            }
+           
+            } 
+            customerLogIn();
+    }
+
+    let logInUIControls = null;
+    switch(logInSetUpState) {
+        case "idle":
+            logInUIControls =<button type="submit">Sign in</button>
+            break;
+        case "processing": 
+        logInUIControls = <span style={{color:"blue"}}>Processing...</span>
+            break;
+        case "SignInSuccessful":
+            logInUIControls = <span style={{color:"green"}}>Sign in successful</span>
+            break;
+
+        case "SignInFailed":
+            logInUIControls = <span style={{color:"red"}}>Sign in failed</span>
+            break;
+    }
+
+    return (
+        <div>
+          <br/>
+            <h2>Login</h2>
+            <form onSubmit={ handleLogIn}>
+                    <p>*Email</p>
+                        <input type="text" name="email" placeholder="Enter email"></input>
+                    <p>*Password:</p>
+                        <input type="text" name="password" placeholder="Enter password"></input>
+                    <br/>
+                    <div>
+                       {logInUIControls}
+                    </div>
+                </form>
+        </div>
+    )
+}
+
+export default Loginpage
